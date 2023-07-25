57d3bb66e93846f6de00cd29c4d28b67defb6ea1
Avinash Vyas <avinash1999.vyas@gmail.com>
2023-07-25 11:44:01 +0530

    training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrePareBaseModelConfig, PrepareCallbacksConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrePareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 
 class ConfigurationManager:
     def __init__(
@@ -13,6 +13,7 @@ class ConfigurationManager:
       self.params = read_yaml(params_filepath)
 
       create_directories([self.config.artifacts_root])
+
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
           config = self.config.data_ingestion
@@ -63,3 +64,24 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"chicken-data")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGUMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
