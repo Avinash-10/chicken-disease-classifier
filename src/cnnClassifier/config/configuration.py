edb81c03470fd0d32a38e1a32fe23133defe2ffb
Avinash Vyas <avinash1999.vyas@gmail.com>
2023-07-26 11:19:25 +0530

    dvc added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrePareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, EvaluationConfig, PrePareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 
 class ConfigurationManager:
     def __init__(
@@ -85,3 +86,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model = Path("artifacts/training/model.h5"),
+            training_data = Path("artifacts/data_ingestion/chicken-data"),
+            all_params = self.params,
+            params_image_size = self.params.IMAGE_SIZE,
+            params_batch_size = self.params.BATCH_SIZE
+        )
+        return eval_config
